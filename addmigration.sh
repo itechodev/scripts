22770fa25640a33d19df8af6bc028e20cc5b6c84
Arie Roos <44861268+arie-itecho@users.noreply.github.com>
2019-09-25 07:34:00 +0200

    Improve resetting migration files

--- addmigration.sh
+++ addmigration.sh
@@ -1,8 +1,7 @@
 # Cleans your migration files and adds a new migration
 # Run without a name argument to only clean your migration files
 echo "Resetting Migration directory to latest commit"
-rm -rf Migrations
-git checkout Migrations
+git checkout HEAD -- Migrations
 if [ -n "$1" ]
 then
     echo "Adding migration $1"
